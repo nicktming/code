06ee4047d3385d09c56b27e499279b38d5f1e835
nicktming <2279563278@qq.com>
2018-10-24 11:07:19 +0800

    update linkedblockingdeque

---
+++ JavaBasics/src/com/linkedblockingdeque/Test01.java
@@ -0,0 +1,9 @@
+package com.linkedblockingdeque;
+
+public class Test01 {
+    public static void main(String[] args) {
+        LinkedBlockingDeque<String> lbd = new LinkedBlockingDeque<>();
+        lbd.add("1");
+        lbd.add("2");
+    }
+}
