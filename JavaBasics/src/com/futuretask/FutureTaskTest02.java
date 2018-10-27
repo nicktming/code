228ddf41bfdfbe42babab4e31d81b9273c49e92f
nicktming <2279563278@qq.com>
2018-10-27 15:12:13 +0800

    FutureTaskTest02

---
+++ JavaBasics/src/com/futuretask/FutureTaskTest02.java
@@ -0,0 +1,28 @@
+package com.futuretask;
+
+import java.util.concurrent.Callable;
+
+public class FutureTaskTest02 {
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask< >(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(Thread.currentThread().getName() + " starts to run.");
+                Thread.sleep(5000);
+                System.out.println(Thread.currentThread().getName() + " wakes up.");
+                return "futurecall";
+            }
+        });
+
+        Thread thread = new Thread(futureTask);
+
+        thread.start();
+
+        System.out.println(Thread.currentThread().getName() + " finished to start thread.");
+
+        System.out.println(Thread.currentThread().getName() + "->" + futureTask.get());
+
+
+
+    }
+}
